fe81963c90450b27f1dacfbdb02351faf2313f74
Ben Shutt <shuttbenjamin@gmail.com>
2024-09-05 18:18:27 +0100

    Update garmin.py

--- Garmin/src/garmin.py
+++ Garmin/src/garmin.py
@@ -14,6 +14,17 @@ from garminconnect import Garmin
 # Number of since now to fetch activities
 DAYS_AGO = 3
 
+# GMT date format of the Garmin models
+GARMIN_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+# Date format to print
+DATE_FORMAT = "%A %d of %m %Y"
+
+# Map a Garmin date format to a date format to print
+def formatted_date_time(date_time_string):
+    instant = datetime.datetime.strptime(date_time_string, GARMIN_DATE_FORMAT)
+    return instant.strftime(DATE_FORMAT)
+
 # User must provide a username and password for authentication as
 # command-line arguments.
 file_name = os.path.basename(sys.argv[0])
@@ -38,6 +49,5 @@ for activity in activities:
     activity_id = activity["activityId"]
     activity_name = activity["activityName"]
     activity_type = activity["activityType"]["typeKey"]
-    activity_date_string = activity["startTimeGMT"]
-    activity_date = datetime.datetime.strptime(activity_date_string, "%Y-%m-%d %H:%M:%S")
+    activity_date = formatted_date_time(activity["startTimeGMT"])
     print(f"activity_id={activity_id}, activity_name={activity_name}, activity_type={activity_type}, activity_date={activity_date}")
