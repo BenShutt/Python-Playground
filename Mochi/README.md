75a69f7465e33082fef2cf79e2b094585b789c1f
Ben Shutt <shuttbenjamin@gmail.com>
2024-09-20 17:34:22 +0100

    Implement main.py of Mochi

---
+++ Mochi/README.md
@@ -0,0 +1,35 @@
+# Mochi
+
+Fetch Mochi images
+
+## Dependencies
+
+The project uses [requests](https://pypi.org/project/requests).
+
+## Installation
+
+Set up virtual environment (VE):
+
+```bash
+python3 -m venv ./venv
+```
+
+Activate the VE:
+
+```bash
+source ./venv/bin/activate
+```
+
+Install dependencies
+
+```bash
+pip3 install -r ./requirements.txt
+```
+
+## Run
+
+In an active VE, run the program
+
+```bash
+python main.py
+```
---
+++ Mochi/main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+"""
+Downloads Mochi images, converts `.webp` to `.png`, and crops
+"""
+
+import requests
+import pathlib
+import os
+import subprocess
+import shutil
+
+# User's home directory
+HOME = str(pathlib.Path.home())
+
+# Path to write images
+DIRECTORY = f"{HOME}/Desktop/MochiImages"
+
+images = {
+    "Caramel": "https://littlemoons.com/wp-content/uploads/2023/09/1_411b0910-1a23-4714-a106-4679cbfd908c.webp",
+    "Chocolate": "https://littlemoons.com/wp-content/uploads/2023/09/1_ChocHazelnut.webp",
+    "Coconut": "https://littlemoons.com/wp-content/uploads/2023/09/1_coconut.webp",
+    "Mango": "https://littlemoons.com/wp-content/uploads/2023/09/1.webp",
+    "Pistachio": "https://littlemoons.com/wp-content/uploads/2023/09/1_Pistach.webp",
+    "Vanilla": "https://littlemoons.com/wp-content/uploads/2023/09/Vanilla_Packshot_1.png"
+}
+
+def init_directory():
+    if not os.path.isdir(DIRECTORY):
+        pathlib.Path(DIRECTORY).mkdir(parents=True, exist_ok=True)
+    else:
+        rc = input(f"{DIRECTORY} already exists, delete? ").lower()
+        if rc in {"yes", "y"}:
+            shutil.rmtree(DIRECTORY)
+            init_directory()
+        else:
+            print("Exiting...")
+            exit(0)
+
+def download_images():
+    for key, value in images.items():
+        extension = pathlib.Path(value).suffix
+        file = f"{DIRECTORY}/Mochi{key}{extension}"
+        data = requests.get(value).content
+        with open(file, "wb") as handler:
+            handler.write(data)
+            
+def run_command(command):
+    if subprocess.call(command, shell=True) != 0:
+        raise SystemExit(f"'{command}' command failed")
+            
+def convert_to_jpg(entry):
+    from_url = entry.path
+    to_url = f"{os.path.splitext(from_url)[0]}.png"
+    command = f"magick {from_url} {to_url}"
+    run_command(command)
+    os.remove(from_url)
+    return to_url
+                
+def process():
+    with os.scandir(DIRECTORY) as it:
+        for entry in it:
+            path = entry.path
+            if entry.is_file() and path.endswith(".webp"):
+                path = convert_to_jpg(entry)
+            
+            if path.endswith(".png"):
+                command = f"magick {path} -crop 750x500+325+550 {path}"
+                run_command(command)
+
+def main():
+    init_directory()
+    download_images()
+    process()
+
+    print(f"Images written to '{DIRECTORY}'")
+
+if __name__ == "__main__":
+    main()
